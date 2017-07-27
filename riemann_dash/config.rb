4f1a408f75c0a74a663a4acf577e87e6cdea78e0
Konrad Kühne <konnykuehne@gmail.com>
2017-07-27 17:31:31 +0200

    remove riemann

--- riemann_dash/config.rb
+++
@@ -1,5 +0,0 @@
-# Serve HTTP traffic on this port
-set  :port, 4567
-
-# Answer queries sent to this IP address
-set :bind, "0.0.0.0"
--- riemann_health/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-riemann-health --host ${RIEMANN_HEALTH_SERVER_HOSTNAME} --event-host $(cat /etc/hostname)
